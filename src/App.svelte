dc045f2bf05c6c924aa1504a699bdc5386c18c50
Itsca <itscae@gmail.com>
2019-10-13 13:14:17 -0600

    • First commit with first component

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+	import Product from './Product.svelte'
+
+	// export let name;
+	let title = 'Starting title';
+	let price = 0;
+	let description = '';
+
+	function setTitle(event) {
+		title = event.target.value;
+	}
+</script>
+
+<style>
+	section {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+	}
+	label, input, textarea {
+		width: 100%;
+	}
+</style>
+
+<p>
+	{title}
+</p>
+<p>
+	{price}
+</p>
+<section>
+	<div>
+		<div>
+			<label for="title">Title</label>
+			<input type="text" it="title" value={title} on:input={setTitle} >
+		</div>
+		<div>
+			<label for="price">Price</label>
+			<input type="number" id="price" value={price} bind:value={price} >
+		</div>
+		<div>
+			<label for="description">Description</label>
+			<textarea name="" id="description " cols="30" rows="3" value={description}></textarea>
+		</div>
+	</div>
+</section>
+
+<Product />
